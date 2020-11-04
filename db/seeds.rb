6e41f7e43d35b4dea7b7678c34b1f3acf287e840
marine <marinekouadio@gmail.com>
2020-11-04 10:47:49 +0100

    seconc

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,35 +10,42 @@
 require 'faker'
 
 
-100.times do |x|
+nb_user = 20
+nb_events = 10
+nb_guests = 20
+
+
+5.times do
   User.create(
-      email: Faker::Name.first_name+'@yopmail.com',
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      description: "moi moi moi moi")
-  puts "Seeding of User nb #{x}"
-end
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    description: Faker::Lorem.paragraphs(number: 1),
+    password: "123456"
+  )
+end 
+#for the random startdate
+t1 = Time.parse("2019-11-01 14:40:34")
+t2 = Time.parse("2022-01-01 00:00:00")
 
-# #for the random startdate
-# t1 = Time.parse("2019-11-01 14:40:34")
-# t2 = Time.parse("2022-01-01 00:00:00")
-
-50.times do |x|
+nb_events.times do |x|
   Event.create(
-    duration: rand(5..100),
-    description: "bla bla bla bla",
+    start_date: rand(t1..t2),
+    duration: rand(5..100)*5,
+    description: Faker::Lorem.paragraph_by_chars(number: 300, supplemental: false),
     location: Faker::Address.city,
     price: rand(1..1000),
-    title: Faker::Book.title )
+    title: Faker::Book.title,
+    admin_id: User.all.sample.id)
   puts "Seeding of Event nb #{x}"
 end
 
-# nb_guests.times do |x|
-#   Attendance.create(
-#     user_id: User.all.sample.id,
-#     event_id: Event.all.sample.id)
-#   puts "Seeding of Guest nb #{x}"
-# end
+nb_guests.times do |x|
+  Attendance.create(
+    user_id: User.all.sample.id,
+    event_id: Event.all.sample.id)
+  puts "Seeding of Guest nb #{x}"
+end
 
 
 
